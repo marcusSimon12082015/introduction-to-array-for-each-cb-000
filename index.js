9946e09a0d2ef8110094af7e7f2aba8eb5babe51
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-01-28 22:21:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function doToElementsInArray(array,callback){
   array.forEach(callback);
 }
-
-
-
 // Add your changeCompletely() function here:
+function changeCompletely(){
+  element.substring(1,4);
+}
